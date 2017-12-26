c93594c66de5d51f5b5954e0a4dcdb962ce930c8
horiz0n-zero <antoinefeufeu@gmail.co>
2017-12-26 23:16:45 +0100

    live, turn and size

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cellular.c
 some experiments before real CA for my new game
+clang -fsanitize=address -g -O0 cellular.c -o cellular
+
+./cellular 'size + 2 for edges' 'turn' 'survive value'
+
+nice exemple :
+./cellular 42 5 4
---
+++ cellular.c
@@ -0,0 +1,112 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+# define CLEAR write(STDOUT_FILENO, "\33[2J\33[0;0H", sizeof("\33[2J\33[0;0H"))
+
+static void				 ft_cellular_print(int *const setting, int **const map)
+{
+	static const char 	*g_status[] =
+	{
+		"\33[37m0 ",
+		"\33[38;2;0;255;255m1 "
+	};
+	int 				x;
+	int 				y;
+	int 				f;
+
+	x = 1;
+	y = 1;
+	CLEAR;
+	while (y < *setting - 1)
+	{
+		while (x < *setting - 1)
+		{
+			f = map[x][y];
+			write(STDOUT_FILENO, g_status[f], strlen(g_status[f]));
+			x++;
+		}
+		write(STDOUT_FILENO, "\n", 1);
+		x = 1;
+		y++;
+	}
+}
+
+static void 			*get_cellular_map(int *const setting)
+{
+	int 				**new;
+	int 				z;
+	int 				x;
+
+	new = malloc(sizeof(int*) * *setting);
+	z = 0;
+	while (z < *setting)
+	{
+		new[z] = malloc(sizeof(int) * *setting);
+		memset(new[z], 0, sizeof(int) * *setting);
+		x = 0;
+		while (x < *setting)
+		{
+			new[z][x] = (int)arc4random_uniform(2);
+			x++;
+		}
+		z++;
+	}	
+	return (new);
+}
+
+static void 			ft_make_turn(int *const setting, int ***map)
+{
+	int **const 		new = get_cellular_map(setting);
+	int **const 		m = *map;
+	int 				x;
+	int 				y;
+	int 				total;
+
+	x = 1;
+	y = 1;
+	while (y < *setting - 1)
+	{
+		while (x < *setting - 1)
+		{
+			total = m[x + 1][y + 1] + m[x + 1][y] + m[x + 1][y - 1] +
+					m[x][y + 1] + m[x][y] + m[x][y - 1] +
+					m[x - 1][y + 1] + m[x - 1][y] + m[x - 1][y - 1];
+			if (total > setting[2])
+				new[x][y] = 1;
+			else
+				new[x][y] = 0;
+			x++;
+		}
+		x = 1;
+		y++;
+	}
+	*map = new;
+}
+
+static void				ft_cellular(int *const setting)
+{
+	int 				**map = get_cellular_map(setting);
+
+	**map = 1;
+	while (setting[1]-- > 0)
+	{
+		ft_make_turn(setting, &map);
+		ft_cellular_print(setting, map);
+		sleep(1);
+	}
+}
+
+int 					main(int argc, char **argv)
+{
+	int 				setting[3];
+	int 				index;
+
+	index = 0;
+	while (*++argv)
+	{
+		setting[index++] = atoi(*argv);
+	}
+	CLEAR;
+	ft_cellular(setting);
+}
